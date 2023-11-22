d6fde199b1dab3ac91d7129499e21a871651a3a0
SyDanhVinh <dothanhtungnb602@gmail.com>
2023-11-22 22:46:47 +0700

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-1": "#6C80F6",
+        "primary-2": "#F5DD00",
+        "primary-3": "#2B3990",
+        "primary-4": "#8CE8E2",
+        "primary-5": "#031C6C",
+        "primary-6": "#FBFBFB",
+        "primary-7": "#E6F4FE",
+        "primary-8": "#E6F4FE",
+        "graydient": "#FFFFFF",
+        "neutral-1": "#323232",
+        "neutral-2": "#555555",
+        "neutral-3": "#808080",
+        "neutral-4": "#C5CCDC",
+        "neutral-5": "#E8E8E8",
+        "neutral-6": "#FFFFFF",
+        "accent-1": "#F65555",
+        "accent-2": "#A9E4F6",
+        "accent-3": "#0A2855",
+      },
+    },
+  },
+  plugins: [],
+}
+
